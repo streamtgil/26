9e12608d35c9489667ff3bedf8a3f3f354b9cc6a
streamtgil <peliseriescnelsuarez89@gmail.com>
2026-03-12 13:36:55 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true
+do
+  ffmpeg -re -i "https://d3snfszc9pg25z.cloudfront.net/media/e1739115-9182-4adc-8d19-8eb7fb6fdd3a/master.m3u8" -c copy -f flv "rtmps://dc4-1.rtmp.t.me/s/3478326752:hDvOxl67y1p0d-_e0vWuxw"
+  
+  echo "Stream stopped, restarting in 5 seconds..."
+  sleep 5
+done
