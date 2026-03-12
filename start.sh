8195cdc7055e69e54eccaf94c0e22273f9ea4d62
streamtgil <peliseriescnelsuarez89@gmail.com>
2026-03-12 13:49:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,7 @@
 
 while true
 do
-  ffmpeg -re \
-  -headers "User-Agent: Mozilla/5.0\r\nReferer: https://13tv.co.il/\r\n" \
-  -i "https://d3snfszc9pg25z.cloudfront.net/media/e1739115-9182-4adc-8d19-8eb7fb6fdd3a/master.m3u8" \
-  -c copy -f flv "rtmps://dc4-1.rtmp.t.me/s/3478326752:hDvOxl67y1p0d-_e0vWuxw"
+ffmpeg -re -i "https://d3snfszc9pg25z.cloudfront.net/media/e1739115-9182-4adc-8d19-8eb7fb6fdd3a/playlist_3.m3u8?_uid=a13a27af-1e4e-40dd-96b1-de300f10061b&rK=a9&_did=4008d3f2-b6db-d900-67ae-f29843b7ea09" -c copy -f flv "rtmps://dc4-1.rtmp.t.me/s/3478326752:hDvOxl67y1p0d-_e0vWuxw"
 
-  echo "Stream stopped, restarting in 5 seconds..."
-  sleep 5
+sleep 5
 done
